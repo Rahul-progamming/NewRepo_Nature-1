6347666a9dc62595a1e215604442fee7471c9a38
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2025-05-11 19:55:47 +0530

    Add project files.

---
+++ Nature_WebApplication/Controllers/WebsiteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nature_WebApplication.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}   
