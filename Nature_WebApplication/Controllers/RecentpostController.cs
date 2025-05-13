bcb913aa932ae04a62e206056a2085ec22eb2e46
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2025-05-13 17:57:53 +0530

    Added Admin dashboard and all

---
+++ Nature_WebApplication/Controllers/RecentpostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nature_WebApplication.Controllers
+{
+    public class RecentpostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RecentPost()
+        {
+            return View();
+        }
+    }
+}
